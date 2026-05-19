461da427cce83b301edfa5e33e28c03756e17984
Qanora <lijinze0118@live.com>
2026-05-20 01:18:37 +0800

    fix: NaN-safe conversion, UTC timezone, EN DASH fix (#89)

---
+++ alphascreener/sources/__init__.py
@@ -0,0 +1,8 @@
+"""Data source adapters: yfinance, FMP, TradingAgents.
+
+Issue #89: yfinance adapter.
+"""
+
+from alphascreener.sources.yfinance_adapter import YFinanceAdapter
+
+__all__ = ["YFinanceAdapter"]
